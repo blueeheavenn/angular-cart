1cedabe36983aa72342a8f53bb2599dd731dc2dd
blueeheavenn <rajib.ahmed.chi@gmail.com>
2023-06-24 14:13:23 -0400

    added product details for products

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  constructor( private route:ActivatedRoute){}
+
+  product :Product | undefined
+
+  ngOnInit(){
+       const id:Number  = Number(this.route.snapshot.paramMap.get('id'))
+      this.product= products.find(p=> p.id == id)
+       
+
+
+  }
 
 }
